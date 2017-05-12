c8fa7fca7f6fc95b0e4846f43144f41afdcd0d94
Shijie Sun <ssjlife0111@163.com>
2017-05-12 20:38:31 +0800

    add [22]. dictionary

---
+++ [22]. dictionary_demo/dictionary_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dictionary_demo
+//
+//  Created by Sun Shijie on 2017/5/12.
+//  Copyright © 2017年 Shijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
